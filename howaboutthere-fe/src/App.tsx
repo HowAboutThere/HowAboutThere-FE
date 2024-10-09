405eebcfce8e7e7840199273b80ec33fec4f6b86
nakyoung98 <jay3925@cau.ac.kr>
2024-10-09 17:53:42 +0900

    Chore: React Query 초기 설정

--- howaboutthere-fe/src/App.tsx
+++ howaboutthere-fe/src/App.tsx
@@ -1,5 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
 function App() {
-  return <></>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
