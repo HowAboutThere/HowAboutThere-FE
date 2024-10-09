185133912eb5ec8b537f4c9bdca6f29f22fa9cf6
nakyoung98 <jay3925@cau.ac.kr>
2024-10-10 02:34:18 +0900

    Style: label 폰트 스타일 지정

--- howaboutthere-fe/tailwind.config.js
+++ howaboutthere-fe/tailwind.config.js
@@ -59,7 +59,7 @@ export default {
       typography: {
         DEFAULT: {
           css: {
-            "h1, h2, h3, h4, h5, h6": {
+            "h1, h2, h3, h4, h5, h6, label": {
               fontFamily: "Paperlogy",
               fontWeight: "800",
             },
