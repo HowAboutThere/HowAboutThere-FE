3c1161995216409e15cc40f41ac8ebc2218997de
nakyoung98 <jay3925@cau.ac.kr>
2024-10-09 18:10:13 +0900

    Chore: React Router 초기 설정

--- howaboutthere-fe/src/App.tsx
+++ howaboutthere-fe/src/App.tsx
@@ -1,5 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routers/router";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,6 +14,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
