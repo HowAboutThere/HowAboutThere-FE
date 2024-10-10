544c9fd0a7be97e0ad4c0288352cf7bf32e8e4bb
nakyoung98 <jay3925@cau.ac.kr>
2024-10-10 13:27:20 +0900

    Add: DatePickerWithRange 내 popover로 표시여부 기능 추가

--- howaboutthere-fe/src/components/DatePicker/DatePickerWithRange.tsx
+++ howaboutthere-fe/src/components/DatePicker/DatePickerWithRange.tsx
@@ -1,53 +1,76 @@
 import * as React from "react";
 import { CalendarIcon } from "@radix-ui/react-icons";
-import { addDays, format } from "date-fns";
-import { DateRange } from "react-day-picker";
+import { format } from "date-fns";
+import { DateRange, SelectRangeEventHandler } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
-export function DatePickerWithRange({ className }: React.HTMLAttributes<HTMLDivElement>) {
+import { useWindowSize } from "usehooks-ts";
+
+type DatePickerWithRangeProps = {
+  isPopover?: boolean;
+};
+export function DatePickerWithRange({
+  className,
+  isPopover,
+}: DatePickerWithRangeProps & React.HTMLAttributes<HTMLDivElement>) {
   const [date, setDate] = React.useState<DateRange | undefined>(undefined);
 
   return (
     <div className={cn("max-w-full grid gap-2", className)}>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(" w-full justify-start text-left font-normal", !date && "text-muted-foreground")}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            <p className=" text-ellipsis ">
-              {date?.from ? (
-                date.to ? (
-                  <>
-                    {format(date.from, "yy/MM/dd")} - {format(date.to, "yy/MM/dd")}
-                  </>
+      {isPopover ? (
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              id="date"
+              variant={"outline"}
+              className={cn(" w-full justify-start text-left font-normal", !date && "text-muted-foreground")}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              <p className=" text-ellipsis ">
+                {date?.from ? (
+                  date.to ? (
+                    <>
+                      {format(date.from, "yy/MM/dd")} - {format(date.to, "yy/MM/dd")}
+                    </>
+                  ) : (
+                    format(date.from, "yy/MM/dd")
+                  )
                 ) : (
-                  format(date.from, "yy/MM/dd")
-                )
-              ) : (
-                <span>날짜를 선택해주세요</span>
-              )}
-            </p>
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0" align="start">
-          <Calendar
-            initialFocus
-            mode="range"
-            defaultMonth={date?.from}
-            fromDate={date?.from}
-            selected={date}
-            numberOfMonths={1}
-            onSelect={setDate}
-          />
-        </PopoverContent>
-      </Popover>
+                  <span>날짜를 선택해주세요</span>
+                )}
+              </p>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <InternalCalendar dateRange={date} onSelectChange={setDate} />
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <InternalCalendar dateRange={date} onSelectChange={setDate} />
+      )}
     </div>
+  );
+}
+
+type InternalCalendarProps = {
+  dateRange?: DateRange;
+  onSelectChange: SelectRangeEventHandler;
+};
+function InternalCalendar({ dateRange, onSelectChange }: InternalCalendarProps) {
+  const { width = 0 } = useWindowSize({});
+  return (
+    <Calendar
+      initialFocus
+      mode="range"
+      defaultMonth={dateRange?.from}
+      fromDate={new Date()}
+      selected={dateRange}
+      numberOfMonths={width > 640 ? 2 : 1}
+      onSelect={onSelectChange}
+    />
   );
 }
