9263d518d0088764cba579a640353356d15313da
nakyoung98 <jay3925@cau.ac.kr>
2024-10-11 01:16:44 +0900

    Feat: UIComponents 내 구현 컴포넌트 추가

--- howaboutthere-fe/src/pages/ui-components/UIComponents.tsx
+++ howaboutthere-fe/src/pages/ui-components/UIComponents.tsx
@@ -1,4 +1,6 @@
 import AIScheduleFormCard from "@/components/Card/AIScheduleFormCard";
+import AIScheduleThemeCard from "@/components/Domain/AIScheduleTheme/AIScheduleThemeCard";
+import ScheduleThemeItem from "@/components/Domain/AIScheduleTheme/ScheduleThemeList/ScheduleThemeItem";
 
 export default function UIComponents() {
   return (
@@ -7,6 +9,22 @@ export default function UIComponents() {
       <div className="p-6">
         <h2 className="mb-4 text-rose-500">AIScheduleFormCard</h2>
         <AIScheduleFormCard />
+      </div>
+      <div className="p-6">
+        <h2 className="mb-4 text-rose-500">AIScheduleThemeCard</h2>
+        <AIScheduleThemeCard />
+      </div>
+      <div className="p-6">
+        <h2 className="mb-4 text-rose-500">ScheduleThemeItem</h2>
+        <ScheduleThemeItem
+          country={"태국"}
+          city={"푸켓"}
+          travelType={"해변 휴양"}
+          imgSrc="https://images.unsplash.com/photo-1597246217838-bfc44d6ac746?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          onSelect={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
       </div>
     </div>
   );
