23dc79c07161d75dfb058c9d640b950f4dfe8a73
nakyoung98 <jay3925@cau.ac.kr>
2024-10-12 15:00:20 +0900

    Fix: Map Pin 자식으로 받는 것으로 변경

--- howaboutthere-fe/src/components/Map/Map.tsx
+++ howaboutthere-fe/src/components/Map/Map.tsx
@@ -1,45 +1,24 @@
-import Pin from "@/assets/map/pin.png";
-import { LatLng } from "@/types/location-type";
+import { Map as GoogleMap, MapProps as GoogleMapProps } from "@vis.gl/react-google-maps";
+import { ReactNode } from "react";
 
-import { AdvancedMarker, Map as GoogleMap, MapProps as GoogleMapProps, useMap } from "@vis.gl/react-google-maps";
-import { useEffect, useState } from "react";
+type MapProps = GoogleMapProps &
+  Required<Pick<GoogleMapProps, "id">> & {
+    children?: ReactNode;
+  };
 
-type MapProps = GoogleMapProps & Required<Pick<GoogleMapProps, "id">>;
-export default function Map(props: MapProps) {
-  const map = useMap(props.id);
-  const [center, setCenter] = useState<LatLng>({ lat: 37.566535, lng: 126.977962 });
-
-  useEffect(() => {
-    if (!map) return;
-
-    const listener = map.addListener("center_changed", () => {
-      const center = map.getCenter();
-      if (center) {
-        setCenter({ lat: center.lat(), lng: center.lng() });
-      }
-    });
-
-    return () => {
-      google.maps.event.removeListener(listener);
-    };
-  }, [map]);
-
+export default function Map({ children, ...props }: MapProps) {
   return (
     <GoogleMap
       className="w-full aspect-video"
-      defaultCenter={center}
-      minZoom={10}
+      defaultCenter={{ lat: 37.5665, lng: 126.978 }}
       defaultZoom={11}
-      maxZoom={18}
       mapId={import.meta.env.VITE_GOOGLE_MAP_ID}
       gestureHandling="none"
       disableDefaultUI
       disableDoubleClickZoom
       {...props}
     >
-      <AdvancedMarker position={center}>
-        <img className="w-10 animate-bounce" src={Pin} alt="pin" />
-      </AdvancedMarker>
+      {children}
     </GoogleMap>
   );
 }
