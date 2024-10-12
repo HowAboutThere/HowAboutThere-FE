58e01cb88a7ed232424d7a07c439f6aa596b506a
nakyoung98 <jay3925@cau.ac.kr>
2024-10-12 15:14:09 +0900

    Feat: UIComponents 내 컴포넌트 추가

--- howaboutthere-fe/src/pages/ui-components/UIComponents.tsx
+++ howaboutthere-fe/src/pages/ui-components/UIComponents.tsx
@@ -1,4 +1,6 @@
 import AIScheduleFormCard from "@/components/Domain/AISchedule/AIScheduleForm/AIScheduleFormCard";
+import AIScheduleLocationCard from "@/components/Domain/AISchedule/AIScheduleLocation/AIScheduleLocationCard";
+import ScheduleLocationItem from "@/components/Domain/AISchedule/AIScheduleLocation/ScheduleLocationList/ScheduleLocationItem";
 import AIScheduleThemeCard from "@/components/Domain/AISchedule/AIScheduleTheme/AIScheduleThemeCard";
 import ScheduleThemeItem from "@/components/Domain/AISchedule/AIScheduleTheme/ScheduleThemeList/ScheduleThemeItem";
 
@@ -23,6 +25,28 @@ export default function UIComponents() {
           imgSrc="https://images.unsplash.com/photo-1597246217838-bfc44d6ac746?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           onSelect={function (): void {
             throw new Error("Function not implemented.");
+          }}
+        />
+      </div>
+      <div className="p-6">
+        <h2 className="mb-4 text-rose-500">AIScheduleLocationCard</h2>
+        <AIScheduleLocationCard />
+      </div>
+      <div className="p-6">
+        <h2 className="mb-4 text-rose-500">ScheduleLocationItem</h2>
+        <ScheduleLocationItem
+          onSelect={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          imgSrc="https://images.unsplash.com/photo-1696993545232-2b2717676c40?q=80&w=1392&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          location={"바나힐 썬월드"}
+          address={"Hòa Ninh, Hòa Vang, Da Nang, 베트남"}
+          description={
+            "눈 앞에 펼쳐지는 아찔한 전망과 함께 다양한 놀이기구와 어트랙션을 즐길 수 있는 이색적인 복합 엔터테인먼트 공간입니다"
+          }
+          latlng={{
+            lat: 0,
+            lng: 0,
           }}
         />
       </div>
