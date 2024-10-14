d6ec7cb29c9c8bd8b47daa28d6b459b1ee552322
nakyoung98 <jay3925@cau.ac.kr>
2024-10-15 04:24:21 +0900

    Fix: ScheduleThemeItem 내 Badge 조건부 렌더링 적용

--- howaboutthere-fe/src/components/Domain/AISchedule/AIScheduleTheme/ScheduleThemeList/ScheduleThemeItem.tsx
+++ howaboutthere-fe/src/components/Domain/AISchedule/AIScheduleTheme/ScheduleThemeList/ScheduleThemeItem.tsx
@@ -31,9 +31,11 @@ export default function ScheduleThemeItem({
         </div>
         <div className="flex flex-col items-end sm:flex-row sm:gap-3 sm:items-center">
           <div className="flex flex-row gap-2 items-center">
-            <Badge className="rounded-full h-fit" variant="outline">
-              <span>{country}</span>
-            </Badge>
+            {country && (
+              <Badge className="rounded-full h-fit" variant="outline">
+                <span>{country}</span>
+              </Badge>
+            )}
             <h3 className="text-sky-700">{city}</h3>
           </div>
           <h4 className="text-stone-700">{travelType}</h4>
