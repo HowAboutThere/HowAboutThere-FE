e667e3e6c048393a31c6bc6db314fd28f39b51e5
nakyoung98 <jay3925@cau.ac.kr>
2024-10-15 03:45:15 +0900

    Feat: axios 처리

---
+++ howaboutthere-fe/src/apis/axios.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const BASE_URL = "http://k8s-eksdemogroup-ac5f71b163-971273520.ap-northeast-2.elb.amazonaws.com";
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
