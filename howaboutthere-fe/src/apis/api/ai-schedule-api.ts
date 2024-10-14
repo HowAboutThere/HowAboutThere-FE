6ada6318efa0e9f872a1d159ba182f9326ee3139
nakyoung98 <jay3925@cau.ac.kr>
2024-10-15 04:26:56 +0900

    Feat: postAIScheduleLocation API 정의

--- howaboutthere-fe/src/apis/api/ai-schedule-api.ts
+++ howaboutthere-fe/src/apis/api/ai-schedule-api.ts
@@ -10,7 +10,21 @@ type postAIScheduleThemeAPIResponse = {
   region: string;
   theme: string;
 }[];
-
 export const postAIScheduleTheme = async (
   data: postAIScheduleThemeAPIRequest
 ): Promise<postAIScheduleThemeAPIResponse> => await axiosInstance.post("/trip/theme", data);
+
+type postAIScheduleLocationAPIRequest = {
+  region: string;
+  thema: string;
+};
+type postAIScheduleLocationAPIResponse = {
+  placeId: string;
+  spotname: string;
+  address: string;
+}[];
+export const postAIScheduleLocation = async (
+  data: postAIScheduleLocationAPIRequest
+): Promise<postAIScheduleLocationAPIResponse> => await axiosInstance.post("/trip/spot", data);
+
+export const postAISchedulePlan = async () => await axiosInstance.post("/trip/itinerary", {});
