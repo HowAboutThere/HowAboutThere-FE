dcbad9eb671732cc2589a64cb87c28c52253f59e
nakyoung98 <jay3925@cau.ac.kr>
2024-10-15 06:54:57 +0900

    Add: AIScheduleLocation 및 AISchedulePlan 내 API 연결

--- howaboutthere-fe/src/components/Domain/AISchedule/AISchedulePlan/AISchedulePlanCard.tsx
+++ howaboutthere-fe/src/components/Domain/AISchedule/AISchedulePlan/AISchedulePlanCard.tsx
@@ -1,8 +1,6 @@
 import arrowLeft from "@/assets/icon/arrow-left.svg";
 import arrowRight from "@/assets/icon/arrow-right.svg";
 
-import mocks from "@/mocks/ai-schedule-plan-mock.json";
-
 import Card from "@/components/Card/Card";
 import Map from "@/components/Map/Map";
 import { useMap } from "@vis.gl/react-google-maps";
@@ -14,19 +12,18 @@ import SchedulePlanListItem from "./SchedulePlanList/SchedulePlanListItem";
 import { ActivityPlan, TransportPlan } from "@/types/plan-type";
 import { Button } from "@/components/ui/button";
 import { usePunnel } from "@/hooks/usePunnel";
+import { useAIScheduleStore } from "@/stores/ai-schedule-store";
 
-type PlanAPIResponse = {
-  result: Array<{
-    date: string;
-    description: string;
-    schedule: Array<ActivityPlan | TransportPlan>;
-  }>;
-};
+export type PlanType = {
+  date: string;
+  description: string;
+  schedule: (ActivityPlan | TransportPlan)[];
+}[];
 
 export default function AISchedulePlanCard() {
+  const plans = useAIScheduleStore((state) => state.plan);
   const { getFirstPunnel } = usePunnel();
 
-  const plans: PlanAPIResponse = mocks as PlanAPIResponse;
   const [currentDay, setCurrentDay] = useState(0);
   const [, setCurrentPolyline] = useState<google.maps.Polyline | null>(null);
 
@@ -37,8 +34,8 @@ export default function AISchedulePlanCard() {
   };
 
   useLayoutEffect(() => {
-    if (map && plans?.result[currentDay]?.schedule) {
-      const points = plans.result[currentDay].schedule.filter((plan) => "latlng" in plan).map((plan) => plan.latlng);
+    if (map && plans?.[currentDay]?.schedule) {
+      const points = plans?.[currentDay].schedule.filter((plan) => "latlng" in plan).map((plan) => plan.latlng);
       console.log(points);
       const bounds = getBoundFromPoints(points);
       map.fitBounds(bounds);
@@ -57,21 +54,19 @@ export default function AISchedulePlanCard() {
         return polyline;
       });
     }
-  }, [currentDay, map, plans.result]);
+  }, [currentDay, map, plans]);
 
   return (
     <Card title={"AI 여행 계획"} description={"선택한 여행지를 기반으로 완성한 일정입니다"}>
       <div className="flex flex-col gap-8">
         <div className=" border rounded-xl overflow-hidden">
           <Map id="ai-schedule-plan-map">
-            {plans?.result[currentDay]?.schedule?.map((plan) => {
-              return "latlng" in plan ? (
-                <MapPin key={`${plan.latlng.lat}-${plan.latlng.lng}`} position={plan.latlng} />
-              ) : null;
+            {plans?.[currentDay]?.schedule?.map((plan) => {
+              return "latlng" in plan ? <MapPin key={`${plan.id}`} position={plan.latlng} /> : null;
             })}
           </Map>
         </div>
-        <SchedulePlanSummary summary={plans?.result[currentDay]?.description} />
+        <SchedulePlanSummary summary={plans?.[currentDay]?.description || ""} />
         <article className="relative p-2 flex flex-col gap-4 items-center rounded-lg bg-sky-50 overflow-hidden">
           <div className="flex flex-row items-center justify-between">
             <div className="flex flex-row items-center gap-4">
@@ -85,7 +80,7 @@ export default function AISchedulePlanCard() {
               <h2 className="text-sky-900">{currentDay + 1} 일차</h2>
 
               <Button
-                onClick={() => setCurrentDay((prev) => Math.min(plans.result.length - 1, prev + 1))}
+                onClick={() => setCurrentDay((prev) => Math.min((plans?.length || 1) - 1, prev + 1))}
                 className="w-8 h-8  rounded-full p-0"
                 variant="outline"
               >
@@ -96,7 +91,7 @@ export default function AISchedulePlanCard() {
           <div className="relative w-full ">
             <div className="absolute z-0 top-0 left-1/2 -translate-x-0.5 w-px h-full border border-dotted border-sky-500" />
             <div className="relative  flex flex-col items-center gap-2">
-              {plans?.result[currentDay]?.schedule?.map((plan) => (
+              {plans?.[currentDay]?.schedule?.map((plan) => (
                 <SchedulePlanListItem key={plan.id} {...plan} />
               ))}
             </div>
