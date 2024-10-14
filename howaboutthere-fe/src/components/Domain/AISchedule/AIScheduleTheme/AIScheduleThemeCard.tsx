10418242ecd70af291c98b0d8d164ce0002fad33
nakyoung98 <jay3925@cau.ac.kr>
2024-10-15 06:09:11 +0900

    Add: AIScheduleThemeCard api 연결 및 mock데이터 추가

--- howaboutthere-fe/src/components/Domain/AISchedule/AIScheduleTheme/AIScheduleThemeCard.tsx
+++ howaboutthere-fe/src/components/Domain/AISchedule/AIScheduleTheme/AIScheduleThemeCard.tsx
@@ -1,4 +1,4 @@
-import mocks from "@/mocks/ai-schedule-theme-mock.json";
+import mocks from "@/mocks/ai-schedule-location-mock.json";
 
 import { useForm } from "react-hook-form";
 
@@ -8,20 +8,72 @@ import Card from "@/components/Card/Card";
 import { Form, FormField } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { usePunnel } from "@/hooks/usePunnel";
+import { useMutation } from "@tanstack/react-query";
+import { getLocationInfo, getLocationPhoto, postAIScheduleLocation } from "@/apis/api/ai-schedule-api";
+import { useAIScheduleStore } from "@/stores/ai-schedule-store";
+import { LocationType } from "@/types/location-type";
+import { useEffect } from "react";
 
-type AIScheduleThemeType = {
+export type AIScheduleThemeType = {
   theme: ThemeType;
 };
 
 export default function AIScheduleThemeCard() {
+  const themes = useAIScheduleStore((state) => state.themes);
+  const setLocations = useAIScheduleStore((state) => state.updateLocations);
+
   const { getNextPunnel } = usePunnel();
 
-  const form = useForm<AIScheduleThemeType>();
-  const themes = mocks;
+  const { mutateAsync, isPending, isError } = useMutation({
+    mutationFn: postAIScheduleLocation,
+  });
 
-  const onClickNext = () => {
+  const form = useForm<AIScheduleThemeType>();
+
+  //TODO:mocks를 이용한 테스트 코드
+  useEffect(() => {
+    const fetchLocationInfo = async () => {
+      if (!isError) return;
+      const locationInfo = await Promise.all(mocks.map((location) => getLocationInfo({ placeId: location.placeId })));
+      // const photos = await Promise.all(
+      //   locationInfo.map((info) => getLocationPhoto({ photo_reference: info.photos[0].photo_reference }))
+      // );
+      const newLocation = mocks.map<LocationType>((location, index) => ({
+        id: index,
+        location: location.spotname,
+        address: location.address,
+        description: "",
+        latlng: { lat: 0, lng: 0 },
+        // description: locationInfo[index].editorial_summary,
+        // latlng: locationInfo[index].geometry.location,
+        // photo: URL.createObjectURL(photos[index]),
+      }));
+      setLocations(newLocation);
+      getNextPunnel();
+    };
+
+    fetchLocationInfo();
+  }, [getNextPunnel, isError, setLocations]);
+
+  const onClickNext = async () => {
     console.log(form.getValues());
-    getNextPunnel();
+    try {
+      const result = await mutateAsync({
+        region: form.getValues("theme").city,
+        thema: form.getValues("theme").travelType,
+      });
+      const newLocation = result.map<LocationType>((location, index) => ({
+        id: index,
+        location: location.spotname,
+        address: location.address,
+        description: "",
+        latlng: { lat: 0, lng: 0 },
+      }));
+      setLocations(newLocation);
+      getNextPunnel();
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -33,11 +85,11 @@ export default function AIScheduleThemeCard() {
             name={"theme"}
             render={({ field }) => (
               <ol className="flex flex-col w-full p-0 gap-1">
-                {themes.result.map((theme) => (
+                {themes.map((theme) => (
                   <ScheduleThemeItem
-                    key={theme.country}
+                    key={theme.id}
                     imgSrc="https://images.unsplash.com/photo-1597246217838-bfc44d6ac746?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    isSelected={field.value && field.value.country === theme.country}
+                    isSelected={field.value && field.value.id === theme.id}
                     onSelect={() => field.onChange(theme)}
                     {...theme}
                   />
@@ -45,7 +97,7 @@ export default function AIScheduleThemeCard() {
               </ol>
             )}
           ></FormField>
-          <Button type="button" onClick={onClickNext}>
+          <Button type="button" onClick={onClickNext} disabled={isPending}>
             다음
           </Button>
         </form>
--- howaboutthere-fe/src/stores/ai-schedule-store.ts
+++ howaboutthere-fe/src/stores/ai-schedule-store.ts
@@ -1,14 +1,17 @@
 import { AIScheduleFormType } from "@/components/Domain/AISchedule/AIScheduleForm/AIScheduleFormCard";
+import { LocationType } from "@/types/location-type";
 import { ThemeType } from "@/types/theme-type";
 import { create } from "zustand";
 
 type State = {
   form: AIScheduleFormType;
   themes: ThemeType[];
+  locations: LocationType[];
 };
 type Action = {
   updateForm: (form: State["form"]) => void;
   updateThemes: (themes: State["themes"]) => void;
+  updateLocations: (Locations: State["locations"]) => void;
 };
 
 export const useAIScheduleStore = create<State & Action>((set) => ({
@@ -18,6 +21,8 @@ export const useAIScheduleStore = create<State & Action>((set) => ({
     region: "domestic",
   },
   themes: [],
+  locations: [],
   updateForm: (form) => set(() => ({ form })),
   updateThemes: (themes) => set(() => ({ themes })),
+  updateLocations: (Locations) => set(() => ({ locations: Locations })),
 }));
