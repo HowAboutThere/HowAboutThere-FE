92f120d014119023a339d312d0b7854fdf6f8c16
nakyoung98 <jay3925@cau.ac.kr>
2024-10-23 19:43:27 +0900

    Fix: BASE_URL 수정
    local, production에 맞게 환경변수 지정

--- howaboutthere-fe/src/apis/axios.ts
+++ howaboutthere-fe/src/apis/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://k8s-eksdemogroup-ac5f71b163-971273520.ap-northeast-2.elb.amazonaws.com";
+const BASE_URL = import.meta.env.VITE_BASE_BACKEND_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
