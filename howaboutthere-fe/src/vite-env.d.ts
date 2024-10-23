bf57087264d8a6c104ecffa925faf949551dc4da
이나경 <66313756+nakyoung98@users.noreply.github.com>
2024-10-23 19:45:10 +0900

    Fix: BASE_URL 수정 (#21)
    local, production에 맞게 환경변수 지정

--- howaboutthere-fe/src/vite-env.d.ts
+++ howaboutthere-fe/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_MAP_API_KEY: string;
   readonly VITE_GOOGLE_MAP_ID: string;
+  readonly VITE_BASE_BACKEND_URL: string;
 }
 
 interface ImportMeta {
